17f35f872997c970553614e825795cfa1c083dec
Clevertop <41929769+Clevertop@users.noreply.github.com>
2024-01-14 14:31:33 +1100

    runs upload now

--- Levels/game_over_screen.gd
+++ Levels/game_over_screen.gd
@@ -1,10 +1,11 @@
 extends CanvasLayer
 
 func _ready():
-	get_tree().paused = true
+	AirtableManager.GameComplete(ScoreManager.score, ScoreManager.playtime)
+	get_tree().paused = true 
 
 func _on_menu_button_button_down():
 	get_tree().paused = false
 	queue_free()
-	AirtableManager.GameComplete(ScoreManager.score, ScoreManager.playtime)
+	
 	get_tree().change_scene_to_file("res://Levels/MainMenu.tscn")
