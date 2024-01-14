5d8cffd17e7c91e02d8ec85066344d1c7787b398
Eefschmeef2310 <ethan.lucasawesome@gmail.com>
2024-01-14 14:37:43 +1100

    Score system added pog

--- Levels/game_over_screen.gd
+++ Levels/game_over_screen.gd
@@ -3,6 +3,8 @@ extends CanvasLayer
 func _ready():
 	get_tree().paused = true
 	AirtableManager.GameComplete(ScoreManager.score, ScoreManager.playtime)
+	
+	$MarginContainer/VBoxContainer/Label4.text = "Orders Completed: " + str(ScoreManager.score)
 
 func _on_menu_button_button_down():
 	get_tree().paused = false
