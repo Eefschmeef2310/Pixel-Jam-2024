0a6d6c3dcb6a152266ce61746a044c477890fd8f
Eefschmeef2310 <ethan.lucasawesome@gmail.com>
2024-01-14 13:12:03 +1100

    updates to order

--- orders/order.gd
+++ orders/order.gd
@@ -7,12 +7,4 @@ var grid: Array
 var width: int
 var height: int
 
-#TODO - I'm assuming that this can run a timer
-var timer = Timer.new()
-var countdown: float = 3.;
-
-func _ready():
-	timer.start(countdown)
-
-func _process(_delta):
-	print(timer.time_left)
+var countdown: float = 10.;
