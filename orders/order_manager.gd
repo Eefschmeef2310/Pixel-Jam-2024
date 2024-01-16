72cdc8ac3065abdfefa14001b1274b08acaa62d6
Cookie <51043100+cookiespl@users.noreply.github.com>
2024-01-16 23:58:37 +1100

    new moving system

--- orders/order_manager.gd
+++ orders/order_manager.gd
@@ -213,6 +213,9 @@ func _on_orders_updated():
 			
 			call(order.type)
 
+func check_all_orders():
+	if grid_node:
+		grid_node.check_all_orders()
 
 func complete_order(order: Order):
 	for o in orders:
